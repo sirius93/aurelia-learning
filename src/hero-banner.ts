b1a2ba0fa491679f25d4f9e4b576f866ecb0f109
Nandan Kumar 2 <nku102@sapient.com>
2018-02-09 15:44:13 +0530

    initial commit

---
+++ src/hero-banner.ts
@@ -0,0 +1,7 @@
+export class HeroBanner {
+  HeroBannerData = {
+    heading : 'Front End Developer',
+    SubHeading : '<em>Hey I am a Nandan</em>',
+    description : 'Specializing in building everything from small business sites to rich interactive web apps.'
+  };
+}
